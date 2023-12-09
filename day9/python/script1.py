ad8162dec7f083a262437d7c2784de76918535fd
Giovanni Enrico Loni <65892886+genricoloni@users.noreply.github.com>
2023-12-09 09:35:47 +0100

    day9 - python for both parts

---
+++ day9/python/script1.py
@@ -0,0 +1,27 @@
+with open("../input.txt") as f:
+    lines = f.read().splitlines()
+
+    tot = 0
+
+    for line in lines:
+        newline = list(line.split(" "))
+        print(newline)
+        
+
+        num = 0
+
+        while any(newline):
+            num += int(newline[-1])
+
+            result = []
+
+            for i in range(1, len(newline)):
+                result.append(int(newline[i]) - int(newline[i - 1]))
+            
+            newline = result
+            print(newline, num)
+            
+
+        tot += num
+
+    print(tot)
---
+++ day9/python/script2.py
@@ -0,0 +1,27 @@
+with open("../input.txt") as f:
+    lines = f.read().splitlines()
+
+    tot = 0
+
+    for line in lines:
+        newline = list(line.split(" "))
+        print(newline)
+    
+        num = 0
+        sign = 1
+
+        while any(newline):
+            num += int(newline[0]) * sign
+            sign *= -1
+
+            result = []
+
+            for i in range(1, len(newline)):
+                result.append(int(newline[i]) - int(newline[i - 1]))
+            
+            newline = result
+            
+
+        tot += num
+
+    print(tot)
